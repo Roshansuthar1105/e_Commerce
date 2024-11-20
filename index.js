a10311c6e2e7119fd05ecebf834c0ba104192312
roshansuthar1105 <roshansuthar2023@gmail.com>
2024-11-20 19:33:39 +0530

    cors implementation

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require('dotenv').config(); // to access .env variables
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./utils/connectDB.js");
 const errorMiddleware = require('./middlewares/errorMiddleware.js');
 const authRouter = require("./routes/authRoute.js");
 const contactRouter = require("./routes/contactRouter.js");
 
 const app = express();
-
-
+// use cors to give access
+const frontendUrl = process.env.FRONTEND_URI || "http://localhost:5173"
+const corsOptions ={
+    origin:frontendUrl,
+    methods:"GET, POST, DELETE, PUT, PATCH, HEAD",
+    credentials:true
+}
+app.use(cors(corsOptions));
 // it is a middleware that parse the data into json type
 app.use(express.json()); // to get json data in all over the application
 
